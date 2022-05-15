4f7fd5f1f41126b5216802035479817c08c18828
Szymon Kępiński <sz.kepinski1@gmail.com>
2022-05-15 18:39:57 +0200

    Can save csv

---
+++ main.py
@@ -0,0 +1,31 @@
+from simulation import Simulation
+import csv
+
+
+def save_frames(frames):
+    id_cur: int
+    with open('simulations_run/independent/current_id.txt', 'r+') as f_txt:
+        id_cur = int(f_txt.read())
+        f_txt.truncate(0)
+        f_txt.seek(0)
+        f_txt.write(str(id_cur+1))
+
+    with open('simulations_run/independent/data{id}.csv'.format(id=str(id_cur)), 'w', newline='') as f:
+        # using csv.writer method from CSV package
+        write = csv.writer(f)
+
+        write.writerow(['Height', 'Velocity', 'Power'])
+        write.writerows(frames)
+
+    with open('simulations_run/all_data.csv', 'a', newline='') as f:
+        # using csv.writer method from CSV package
+        write = csv.writer(f)
+
+        write.writerows(frames)
+
+
+if __name__ == '__main__':
+    Simulation.init()
+    result, frames = Simulation.run()
+    if Simulation.rate():
+        save_frames(frames)
---
+++ simulation.py
@@ -0,0 +1,199 @@
+import random
+
+import pygame
+
+from random import randrange
+
+class Colors:
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GRAY = (220, 220, 220)
+
+
+class Constants:
+    # G constant x planet's mass
+    GM = 6.674e-11 * 5.972e24
+
+    # planet's radius
+    R_PLANET = 6.371e6
+
+    # screen size
+    screen_size = (1024, 576)
+
+    # how many meters are visible on the screen
+    height_visible = 1e6
+
+    # meters per pixel
+    meters_per_pixel = height_visible / screen_size[1]
+
+    # dt[s] for frame
+    dt = 0.25
+
+    # where mouse will be put after starting
+    pos_mouse_start = (850, screen_size[1]*0.8)
+
+    # where mouse needs to be in order to start
+    rect_to_start = pygame.Rect(800, screen_size[1]*0.8 - 25, 100, 50)
+
+    # font size
+    font_size = 25
+
+    # percent
+    percent_bot = 0.2
+    percent_top = 0.8
+
+
+class Rocket:
+    size = (0.025 * Constants.screen_size[0], 0.15 * Constants.screen_size[1])
+    max_power = 12.5
+
+    rect: pygame.Rect
+    vel: float
+    height: float
+
+    def __init__(self,  height: float, vel: float):
+        self.height = height
+        self.vel = vel
+        self.update_rect()
+
+    def draw(self, screen: pygame.display):
+        pygame.draw.rect(screen, Colors.RED, self)
+
+    def update(self, normalized_power):
+        r = Constants.R_PLANET + self.height
+        acc_grav = -Constants.GM/(r**2)
+        acc_power = normalized_power * Rocket.max_power
+        self.vel = self.vel + (acc_grav + acc_power) * Constants.dt
+        self.height = self.height + self.vel * Constants.dt
+        self.update_rect()
+
+    def update_rect(self):
+        x_center = Constants.screen_size[0]/2
+        y_center = Constants.screen_size[1] - (self.height / Constants.meters_per_pixel)
+        self.rect = pygame.Rect((x_center-Rocket.size[0]/2, y_center-Rocket.size[1]), Rocket.size)
+
+    @staticmethod
+    def scale_to_power(percent_of_screen):
+        t = Constants.percent_top
+        b = Constants.percent_bot
+        power = percent_of_screen / (t-b) - b/(t-b)
+        if power > 1:
+            return 1
+        if power < 0:
+            return 0
+        return power
+
+
+class Simulation:
+    font: pygame.font
+    fps: int
+    screen: pygame.display
+    clock: pygame.time.Clock
+    rocket: Rocket
+
+    @staticmethod
+    def init():
+        pygame.init()
+        pygame.font.init()
+        Simulation.font = pygame.font.SysFont('Comic Sans MS', Constants.font_size)
+        Simulation.fps = 120
+        Simulation.screen = pygame.display.set_mode(Constants.screen_size)
+        Simulation.clock = pygame.time.Clock()
+        height, vel = Simulation.get_starting_conditions()
+        Simulation.rocket = Rocket(height, vel)
+        Simulation.screen.fill(Colors.WHITE)
+        Simulation.rocket.draw(Simulation.screen)
+        pygame.draw.rect(Simulation.screen, Colors.RED, Constants.rect_to_start)
+        pygame.display.update()
+
+    @staticmethod
+    def run():
+        frames = []
+        time = 0
+
+        cursor_set = False
+        while not cursor_set:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    r = Constants.rect_to_start
+                    if r.x < x < (r.x + r.width) and r.y < y < (r.y + r.height):
+                        cursor_set = True
+        pygame.mouse.set_visible(True)
+        pygame.mouse.set_pos(Constants.pos_mouse_start)
+
+        # start simulation
+        while Simulation.rocket.height > 0:
+            # input
+            x, y = pygame.mouse.get_pos()
+            percent_of_screen = (Constants.screen_size[1] - y) / Constants.screen_size[1]
+            normalized_power = Rocket.scale_to_power(percent_of_screen)
+            frames.append([Simulation.rocket.height, Simulation.rocket.vel, normalized_power])
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return False
+
+            Simulation.screen.fill(Colors.WHITE)
+            Simulation.clock.tick(Simulation.fps)
+            time += Constants.dt
+            Simulation.rocket.update(normalized_power)
+
+            Simulation.adjust_fps()
+
+            velocity = Simulation.font.render('velocity: ' + str(round(Simulation.rocket.vel)), False, (0, 0, 0))
+            height = Simulation.font.render('height: ' + str(round(Simulation.rocket.height)), False, (0, 0, 0))
+            power = Simulation.font.render('power: ' + str(round(normalized_power, 2)), False, (0, 0, 0))
+            fps = Simulation.font.render('fps: ' + str(Simulation.fps), False, (0, 0, 0))
+            Simulation.screen.blit(velocity, (0, Constants.font_size *0))
+            Simulation.screen.blit(height, (0, Constants.font_size*1))
+            Simulation.screen.blit(power, (0, Constants.font_size*2))
+            Simulation.screen.blit(fps, (0, Constants.font_size*3))
+            Simulation.draw_power(normalized_power)
+            Simulation.rocket.draw(Simulation.screen)
+
+            pygame.display.update()
+
+        return Simulation.check_result(), frames
+
+    @staticmethod
+    def check_result():
+        return abs(Simulation.rocket.vel) < 4
+
+    @staticmethod
+    def draw_power(normalized_power):
+        bottom = Constants.screen_size[1] * (1-Constants.percent_bot)
+        max_top = Constants.screen_size[1] * (1-Constants.percent_top)
+        top = bottom - (bottom-max_top) * normalized_power
+        pygame.draw.rect(Simulation.screen, Colors.GRAY, pygame.Rect(800, top, 100, bottom-top))
+
+    @staticmethod
+    def adjust_fps():
+        if Simulation.rocket.height < 1.5e3:
+            Simulation.fps = 8
+        elif Simulation.rocket.height < 3e4:
+            Simulation.fps = 30
+        else:
+            Simulation.fps = 120
+
+    @staticmethod
+    def rate():
+        Simulation.screen.fill(Colors.WHITE)
+        info = Simulation.font.render('g-good, b-bad', False, (0, 0, 0))
+        Simulation.screen.blit(info, (Constants.screen_size[0]/2, Constants.screen_size[1]/2))
+        pygame.display.update()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key ==pygame.K_g:
+                        return True
+                    elif event.key == pygame.K_b:
+                        return False
+
+    @staticmethod
+    def get_starting_conditions():
+        return random.uniform(0.3e6, 0.7e6), random.uniform(-200.0, 100.0)
